d1642a6ad7c055bc86a00ed6960255a56a752932
Ikhwanul Abiyu <ikhwanulabiyu@gmail.com>
2024-08-13 12:03:24 +0700

    feat: adding label for form

--- src/utils/Input.jsx
+++ src/utils/Input.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
-const Input = ({type, title}) => {
+const Input = ({ type, title, label, form, setForm }) => {
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="flex flex-col gap-2">
-      <label className="text-sm font-DevTales text-gray-700">{title}</label>
-      <input 
-        type={type} 
-        name={title.toLowerCase()} 
+      <label className="text-sm font-medium text-gray-700">{label}</label>
+      <input
+        type={type}
+        name={title}
         className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
+        onChange={handleChange}
       />
     </div>
   );
